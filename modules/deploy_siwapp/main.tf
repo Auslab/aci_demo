71b408f47f3710059dd1bec1fdf3eb58cd6eb84b
Brandon Beck <beckbran@gmail.com>
2021-10-05 08:29:27 -0500

    removing web vms

--- modules/deploy_siwapp/main.tf
+++ modules/deploy_siwapp/main.tf
@@ -41,48 +41,6 @@ module "siwapp_haproxy_app" {
   windows_product_key = ""
 }
 
-module "siwapp_web" {
-  count   = 3
-  source  = "app.terraform.io/Auslab/vms/vsphere"
-  version = "0.0.43"
-
-  providers = {
-    vsphere  = vsphere
-    infoblox = infoblox
-    ad       = ad
-  }
-  module_index = count.index
-  vm = {
-    vcenter    = var.vcenter_hostname
-    template   = "Centos_7-Template"
-    name       = "siwapp-aci-web-${count.index + 1}"
-    datacenter = "GeneralPurpose"
-    datastore  = "GeneralPurpose"
-    folder     = "Users/robbeck"
-    cluster    = "fury-hx-cluster"
-    cpu        = 2
-    memory     = 4096
-    disk       = 40
-    network    = var.frontend_network
-    subnet     = var.frontend_subnet
-    os_type    = "linux"
-  }
-  user = {
-    userId    = "robbeck"
-    firstName = "Brandon"
-    lastName  = "Beck"
-  }
-  deployment = {
-    guid    = 8675309
-    name    = "acidemo"
-    project = "acidemo"
-  }
-  domain_admin = {
-    username = ""
-    password = ""
-  }
-  windows_product_key = ""
-}
 module "siwapp_app" {
   count   = 3
   source  = "app.terraform.io/Auslab/vms/vsphere"
@@ -212,5 +170,5 @@ module "siwapp_haproxy_db" {
 }
 
 output "siwapp_vms" {
-  value = [for v in concat(module.siwapp_haproxy_app, module.siwapp_haproxy_db, module.siwapp_web, module.siwapp_app, module.siwapp_db) : v.vm]
+  value = [for v in concat(module.siwapp_haproxy_app, module.siwapp_haproxy_db, module.siwapp_app, module.siwapp_db) : v.vm]
 }
