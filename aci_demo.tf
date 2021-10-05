c2f6169e647e67a9216450cb6e43f4549948559e
Brandon Beck <beckbran@gmail.com>
2021-10-05 11:58:49 -0500

    remove aci policies

--- aci_demo.tf
+++ aci_demo.tf
@@ -1,32 +1,32 @@
-data "aci_tenant" "common" {
-  name = "common"
-}
+# data "aci_tenant" "common" {
+#   name = "common"
+# }
 
-resource "aci_tenant" "terraform_demo" {
-  name = "terraform-demo"
-}
+# resource "aci_tenant" "terraform_demo" {
+#   name = "terraform-demo"
+# }
 
-data "aci_vrf" "prod" {
-  tenant_dn = data.aci_tenant.common.id
-  name      = "Prod"
-}
+# data "aci_vrf" "prod" {
+#   tenant_dn = data.aci_tenant.common.id
+#   name      = "Prod"
+# }
 
-data "aci_l3_outside" "prod_out" {
-  tenant_dn = data.aci_tenant.common.id
-  name      = "prod-l3-out"
-}
+# data "aci_l3_outside" "prod_out" {
+#   tenant_dn = data.aci_tenant.common.id
+#   name      = "prod-l3-out"
+# }
 
-resource "aci_bridge_domain" "demo_1" {
-  tenant_dn                = aci_tenant.terraform_demo.id
-  description              = "Frontend app bd"
-  name                     = "aci-demo-frontend"
-  relation_fv_rs_ctx       = data.aci_vrf.prod.id
-  relation_fv_rs_bd_to_out = [data.aci_l3_outside.prod_out.id]
-}
+# resource "aci_bridge_domain" "demo_1" {
+#   tenant_dn                = aci_tenant.terraform_demo.id
+#   description              = "Frontend app bd"
+#   name                     = "aci-demo-frontend"
+#   relation_fv_rs_ctx       = data.aci_vrf.prod.id
+#   relation_fv_rs_bd_to_out = [data.aci_l3_outside.prod_out.id]
+# }
 
-resource "aci_subnet" "field_hx_net_1_subnet" {
-  parent_dn                       = aci_bridge_domain.demo_1.id
-  description                     = "Frontend app network"
-  ip                              = "172.19.6.1/24"
-  scope                           = ["public"]
-}
+# resource "aci_subnet" "field_hx_net_1_subnet" {
+#   parent_dn                       = aci_bridge_domain.demo_1.id
+#   description                     = "Frontend app network"
+#   ip                              = "172.19.6.1/24"
+#   scope                           = ["public"]
+# }
