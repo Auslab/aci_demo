31c855b7403d5dcaed08652d85a546e7aa8e1d1e
Brandon Beck <beckbran@gmail.com>
2021-10-04 20:50:38 -0500

    added providers

--- siwapp.tf
+++ siwapp.tf
@@ -1,5 +1,10 @@
 module "siwapp" {
   source           = "git::https://github.com/Auslab/aci_demo.git//modules/deploy_siwapp"
+  providers = {
+    vsphere  = vsphere
+    infoblox = infoblox
+    ad       = ad
+  }
   frontend_network = "wwdc-shared|user-networks|user-net-1"
   backend_network  = "wwdc-shared|user-networks|mgmt-net-1"
   frontend_subnet  = "172.18.64.0/20"
