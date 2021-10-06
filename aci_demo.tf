909eaf1b8465b5a46b3db3627c211e28bec2cdab
Brandon Beck <beckbran@gmail.com>
2021-10-06 10:25:52 -0500

    adding aci code

---
+++ aci_demo.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    aci = {
+      source = "CiscoDevNet/aci"
+      version = "0.7.1"
+    }
+  }
+}
+
+provider "aci" {
+  # Configuration options
+}
+
+data "aci_tenant" "common" {
+  name = "common"
+}
+
+output "common_tenant" {
+  value = data.aci_tenant.common.id
+}
+
+resource "aci_tenant" "terraform_demo" {
+  name = "terraform-demo"
+}
+
+data "aci_vrf" "prod" {
+  tenant_dn = data.aci_tenant.common.id
+  name      = "Prod"
+}
+
+data "aci_l3_outside" "prod_out" {
+  tenant_dn = data.aci_tenant.common.id
+  name      = "prod-l3-out"
+}
+
+resource "aci_bridge_domain" "demo_1" {
+  tenant_dn                = aci_tenant.terraform_demo.id
+  description              = "Frontend app bd"
+  name                     = "aci-demo-frontend"
+  unk_mac_ucast_act        = "proxy"
+  arp_flood                = "no"
+  relation_fv_rs_ctx       = data.aci_vrf.prod.id
+  relation_fv_rs_bd_to_out = [data.aci_l3_outside.prod_out.id]
+}
+
+resource "aci_subnet" "field_hx_net_1_subnet" {
+  parent_dn                       = aci_bridge_domain.demo_1.id
+  description                     = "Frontend app network"
+  ip                              = "172.19.6.1/24"
+  scope                           = ["public"]
+}
