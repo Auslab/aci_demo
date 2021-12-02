6ce98ca3cb9febf8fa09e5a9471e68f1d3308650
Brandon Beck <beckbran@gmail.com>
2021-12-02 07:11:01 -0600

    testing

--- providers.tf
+++ providers.tf
@@ -2,22 +2,22 @@ provider "aci" {
   # Configuration options
 }
 
-provider "vsphere" {
-  vsphere_server       = var.vcsa_fury_hostname
-  user                 = var.vcsa_fury_username
-  password             = var.vcsa_fury_password
-  allow_unverified_ssl = true
-}
+# provider "vsphere" {
+#   vsphere_server       = var.vcsa_fury_hostname
+#   user                 = var.vcsa_fury_username
+#   password             = var.vcsa_fury_password
+#   allow_unverified_ssl = true
+# }
 
-provider "ad" {
-  winrm_hostname = var.ad_infra.hostname
-  winrm_username = var.ad_infra.username
-  winrm_password = var.ad_infra.password
-  winrm_insecure = true
-  winrm_port     = 5986
-  winrm_proto    = "https"
-}
+# provider "ad" {
+#   winrm_hostname = var.ad_infra.hostname
+#   winrm_username = var.ad_infra.username
+#   winrm_password = var.ad_infra.password
+#   winrm_insecure = true
+#   winrm_port     = 5986
+#   winrm_proto    = "https"
+# }
 
-provider "infoblox" {
-    disable_tls_verification = true
-}
+# provider "infoblox" {
+#     disable_tls_verification = true
+# }
--- versions.tf
+++ versions.tf
@@ -4,17 +4,17 @@ terraform {
       source = "CiscoDevNet/aci"
       version = "0.7.1"
     }
-    vsphere = {
-      source = "hashicorp/vsphere"
-      version = "2.0.2"
-    }
-    infoblox = {
-      source = "techBeck03/infoblox"
-      version = "2.0.7"
-    }
-    ad = {
-      source = "hashicorp/ad"
-      version = "0.4.3"
-    }
+    # vsphere = {
+    #   source = "hashicorp/vsphere"
+    #   version = "2.0.2"
+    # }
+    # infoblox = {
+    #   source = "techBeck03/infoblox"
+    #   version = "2.0.7"
+    # }
+    # ad = {
+    #   source = "hashicorp/ad"
+    #   version = "0.4.3"
+    # }
   }
 }
