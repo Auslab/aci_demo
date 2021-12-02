1bb06a6c71f42477eba95c787081d5ebee4275bc
Brandon Beck <beckbran@gmail.com>
2021-12-02 09:15:59 -0600

    format testing

--- paths.tf
+++ paths.tf
@@ -19,19 +19,23 @@ variable "aci_paths" {
   type = string
 }
 
-module "vpc_static_paths" {
-  for_each = { for path in csvdecode(var.aci_paths) : "${path.vpc_name}_${path.tenant_name}_${path.anp_name}_${path.epg_name}_${path.vlan_id}" => path }
-  source   = "./modules/static_path"
-
-  vpc_name    = each.value.vpc_name
-  tenant_name = each.value.tenant_name
-  anp_name    = each.value.anp_name
-  epg_name    = each.value.epg_name
-  leaf_a_id   = each.value.leaf_a_id
-  leaf_b_id   = each.value.leaf_b_id
-  vlan_id     = each.value.vlan_id
+output "formatted_csv" {
+  value = replace(var.aci_paths, "\\\\n", "\n")
 }
 
-output "vpc_static_path_dns" {
-  value = [for path in module.vpc_static_paths : path.path_dn]
-}
+# module "vpc_static_paths" {
+#   for_each = { for path in csvdecode(replace(var.aci_paths, "\\\\n", "\n")) : "${path.vpc_name}_${path.tenant_name}_${path.anp_name}_${path.epg_name}_${path.vlan_id}" => path }
+#   source   = "./modules/static_path"
+
+#   vpc_name    = each.value.vpc_name
+#   tenant_name = each.value.tenant_name
+#   anp_name    = each.value.anp_name
+#   epg_name    = each.value.epg_name
+#   leaf_a_id   = each.value.leaf_a_id
+#   leaf_b_id   = each.value.leaf_b_id
+#   vlan_id     = each.value.vlan_id
+# }
+
+# output "vpc_static_path_dns" {
+#   value = [for path in module.vpc_static_paths : path.path_dn]
+# }
